fc42280199f79aa01aefed34e054dfe9ba2cbbfe
David McCaffrey <dmccaffrey@vmware.com>
2017-01-30 17:58:16 -0600

    IT VERKS

--- test.py
+++ test.py
@@ -3,14 +3,35 @@ import socket
 import ssl
 import time
 import sys
+import fcntl
+import os
+import struct
+import subprocess
 from pprint import pprint
 
-PATH="/home/redx/dev/tls-test/"
+PATH="/home/redx/tls-test/"
 CA_CERTS_FILE = PATH + "ca/intermediate/certs/ca-chain.cert.pem"
 SERVER_CERT = PATH + "ca/intermediate/certs/alpha.vmware.local.server.cert.pem"
 SERVER_KEY = PATH + "ca/intermediate/private/alpha.vmware.local.key.pem"
 CLIENT_CERT = PATH + "ca/intermediate/certs/bravo.vmware.local.usr.cert.pem"
 CLIENT_KEY = PATH + "ca/intermediate/private/bravo.vmware.local.key.pem"
+
+def create_tun(name, ip):
+    TUNSETIFF = 0x400454ca
+    TUNSETOWNER = TUNSETIFF + 2
+    IFF_TUN = 0x0001
+    IFF_TAP = 0x0002
+    IFF_NO_PI = 0x1000
+    tun = open('/dev/net/tun', 'r+b')
+    ifr = struct.pack('16sH', name, IFF_TUN | IFF_NO_PI)
+    fcntl.ioctl(tun, TUNSETIFF, ifr)
+    fcntl.ioctl(tun, TUNSETOWNER, 1000)
+    subprocess.check_call('ip addr change dev {} {}'.format(name, ip),
+                          shell=True)
+    subprocess.check_call('ip link set dev {} up'.format(name),
+                          shell=True)
+    return tun
+
 
 def connect(hostname, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
@@ -33,13 +54,16 @@ def dump_info(socket):
     pprint(socket.getpeercert())
 
 def run_client():
+    tun = create_tun("client0", "10.0.0.1/24")
     s_sock = connect('localhost', 2243)
     dump_info(s_sock)
     while True:
-        time.sleep(1)
-        s_sock.write(b"test");
+        packet = os.read(tun.fileno(), 4096)
+        print("got paket")
+        s_sock.write(packet)
 
 def run_server():
+    tun = create_tun("server0", "10.0.0.2/24")
     sock = listen(2243)
     while True:
         n_sock, addr = sock.accept()
@@ -47,13 +71,16 @@ def run_server():
         dump_info(stream)
         try:
             while True:
-                print("Received: ", stream.read())
+                packet = stream.read()
+                print("got paket")
+                os.write(tun.fileno(), packet)
         finally:
             stream.shutdown(socket.SHUT_RDWR)
             stream.close()
 
 if __name__ == "__main__":
-    if sys.argv[1] == "client":
+    type = sys.argv[1]
+    if type == "client":
         run_client()
-    else:
+    elif type == "server":
         run_server()
